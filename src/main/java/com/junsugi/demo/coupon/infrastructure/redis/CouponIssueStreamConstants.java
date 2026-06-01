ccfe01a82834e5476fb508a58a6e56ec88962754
junsugi <wnstjrl96@naver.com>
2026-06-01 14:13:30 +0900

    Pending 워커 설정 추가

--- src/main/java/com/junsugi/demo/coupon/infrastructure/redis/CouponIssueStreamConstants.java
+++ src/main/java/com/junsugi/demo/coupon/infrastructure/redis/CouponIssueStreamConstants.java
@@ -5,5 +5,7 @@ public final class CouponIssueStreamConstants {
     public static final String ISSUE_STREAM_KEY = "coupon:issue:stream";
     public static final String ISSUE_GROUP = "coupon-issue-group";
 
+    public static final String ISSUE_PENDING_WORKER_CONSUMER = "coupon-issue-pending-worker-1";
+
     private CouponIssueStreamConstants() {}
 }
